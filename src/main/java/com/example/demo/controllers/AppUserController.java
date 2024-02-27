b80ea4dbae9df71b047fdc79bec09f9daa0e15d1
Josefy Vladimir <st55776@upce.cz>
2024-02-27 15:33:44 +0100

    return JSON

--- src/main/java/com/example/demo/controllers/AppUserController.java
+++ src/main/java/com/example/demo/controllers/AppUserController.java
@@ -3,21 +3,24 @@ package com.example.demo.controllers;
 import com.example.demo.dao.AppUser;
 import com.example.demo.repository.AppUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.awt.*;
 import java.util.List;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppUserController {
     @Autowired
     private AppUserRepository appUserRepository;
-    @GetMapping("/users")
-    public String listAll(Model model) {
+    @GetMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody List<AppUser> listAll(Model model) {
         List<AppUser> users = appUserRepository.findAll();
         model.addAttribute("users", users);
-
-        return "users";
+        return users;
     }
 }
